a80b86aaf34972720996471beb271704ac6a30ec
Thomas Lorentzen <thomas@tlorentzen.net>
2022-03-29 11:14:30 +0200

    Mono

--- processing/audio.py
+++ processing/audio.py
@@ -6,7 +6,7 @@ import numpy as np
 
 def load(path):
     if os.path.exists(path):
-        time_series, sampling_rate = librosa.load(path, sr=None)
+        time_series, sampling_rate = librosa.load(path, sr=None, mono=True)
         return Audio(path, time_series, sampling_rate)
     else:
         return None
